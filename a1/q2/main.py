9573b5f257f8f4acddc7eab3eb81286b8e515805
Liam Ozog <lozog@uwaterloo.ca>
2017-05-14 17:41:05 -0400

    feat: read puzzles in from file

--- a1/q2/main.py
+++ a1/q2/main.py
@@ -1,1 +1,17 @@
-print("hello, world!")
+# usage: python main.py <inputfile>
+
+import sys
+
+# parse command line arguments
+inputFile = "easy.txt"
+if len(sys.argv) > 1:
+    inputFile = sys.argv[1] + ".txt"
+
+# read input from file to list
+puzzle = []
+sudokuInput = open(inputFile)
+for line in sudokuInput:
+    puzzle.append(line.rstrip().split(','))
+
+for row in puzzle:
+    print row
