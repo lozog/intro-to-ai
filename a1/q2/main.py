7fac28ca956774329aeb929ae4e73208eba7bd38
Liam Ozog <lozog@uwaterloo.ca>
2017-05-16 15:17:35 -0400

    feat: puzzle.display() method feat: puzzle.selectVariable() now removes selected cell from empty list feat: if puzzle not consistent, puzzle.inConsistent() now resets the cell

--- a1/q2/main.py
+++ a1/q2/main.py
@@ -10,13 +10,32 @@ class Puzzle(object):
         self.empty = []
         self.domain = {1,2,3,4,5,6,7,8,9}
 
+    def display(self):
+        print "    0 1 2 3 4 5 6 7 8\n"
+
+        i = 0
+        for row in self.assignments:
+            print i,' ',
+            i += 1
+            for value in row:
+                if value == '':
+                    print '_',
+                else:
+                    print value,
+            print '\n',
+
     def setCell(self, cell, value):
         self.assignments[cell[0]][cell[1]] = value
+        if value == '':
+            self.empty.append(cell)
 
     # selects which cell to fill next
     def selectVariable(self):
         # return (0,0)
-        return self.empty.pop( randint(0, len(self.empty)) )
+        numEmpty = len(self.empty)
+        print "numEmpty: " + str(numEmpty)
+        # return self.empty.pop( randint(0, numEmpty) )
+        return self.empty.pop(  )
 
     # checks if entire puzzle is filled out
     def isComplete(self):
@@ -26,16 +45,25 @@ class Puzzle(object):
     def isConsistent(self, selectedCell, value):
         x = selectedCell[0]
         y = selectedCell[1]
+        print "selected cell:", int(x), int(y)
         self.assignments[x][y] = value
+
+        if selectedCell == (8,5):
+            print "(8,5) value: ", value
 
         # check alldiff of each row
         for r in range(9):
-            row = [x for x in self.assignments[r] if x != ''] # get # of values in row - ignore empty spaces, they have no impact on consistency
+            row = [i for i in self.assignments[r] if i != ''] # get # of values in row - ignore empty spaces, they have no impact on consistency
             rowSet = set(row) # get # of unique values in row
             if len(row) != len(rowSet):
                 # print row
                 # print rowSet
+                print "row overwrite:", int(x), int(y)
+                self.assignments[x][y] = ''
                 return False
+
+        if selectedCell == (8,5):
+            print "(8,5) value: ", value
 
         # check alldiff of each column
         for c in range(9):
@@ -44,7 +72,12 @@ class Puzzle(object):
             if len(col) != len(colSet):
                 # print col
                 # print colSet
+                print "col overwrite:", int(x), int(y)
+                self.assignments[x][y] = ''
                 return False
+
+        if selectedCell == (8,5):
+            print "(8,5) value: ", value
 
         # check alldiff of each block
         # (blockX, blockY) is the top-left most cell of each block
@@ -63,6 +96,8 @@ class Puzzle(object):
                 if len(block) != len(blockSet):
                     # print block
                     # print blockSet
+                    print "block overwrite:", int(x), int(y)
+                    self.assignments[x][y] = ''
                     return False
 
         return True
@@ -70,23 +105,30 @@ class Puzzle(object):
 def backtrackingSearch(puzzle):
     if puzzle.isComplete():
         return puzzle
+
     selectedCell = puzzle.selectVariable()
+
     print "cell: ",
     print selectedCell
 
     for value in puzzle.domain:
         print "checking value: " + str(value)
         if puzzle.isConsistent(selectedCell, value):
-            print "consistent!"
+
+            puzzle.display()
 
             puzzle.setCell(selectedCell, value)
 
-            result = backtrackingSearch(puzzle)
+            # OKAY I THINK it's passing puzzle by REFERENCE
+            # but does that matter?
+            result = backtrackingSearch( puzzle )
 
             if result != False:
                 return result
 
+            print 'damn, we fucked up somewhere. currentval: ', str(value)
             puzzle.setCell(selectedCell, '')
+            puzzle.display()
     return False
 
 puzzle = Puzzle()
@@ -111,14 +153,12 @@ for row in puzzle.assignments:
         else:
             puzzle.setCell((r, c), int(value)) # convert values to ints
         c += 1
-    print row
     r += 1
 
 # main
-completedPuzzle = backtrackingSearch(puzzle)
+completedPuzzle = backtrackingSearch( puzzle )
 
 if completedPuzzle != False:
-    for row in completedPuzzle:
-        print row
+    completedPuzzle.display()
 else:
     print "could not complete puzzle"
