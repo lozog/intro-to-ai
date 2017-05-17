1ba1c2ee893b015d912d56c6862ca45565310da2
Liam Ozog <lozog@uwaterloo.ca>
2017-05-17 17:02:27 -0400

    feat: keep track of remaining possible values of each cell for forward checking

--- a1/q2/main.py
+++ a1/q2/main.py
@@ -1,18 +1,31 @@
-# usage: python main.py <inputfile>
+# usage: python main.py <forwardCheck> <inputfile>
 
 import sys
+from math import floor
+from copy import deepcopy
 from random import randint
 
 class Puzzle(object):
 
-    def __init__(self):
+    def __init__(self, forwardCheck):
         self.assignments = []
         self.empty = []
         self.domain = {1,2,3,4,5,6,7,8,9}
+        self.remaining = []
+        self.forwardCheck = forwardCheck
 
     def fill(self, inputPuzzle):
         for line in inputPuzzle:
-            puzzle.assignments.append(line.rstrip().split(','))
+            self.assignments.append(line.rstrip().split(','))
+
+        if puzzle.forwardCheck:
+            # fill grid of remaining possible values for forward checking
+            for r in range(9):
+                self.remaining.append([])
+                for c in range(9):
+                    self.remaining[r].append(deepcopy(self.domain)) # create a copy in each cell in self.remaining
+                # for
+            # for
 
         r = 0
         for row in self.assignments:
@@ -23,11 +36,37 @@ class Puzzle(object):
                     self.empty.append(cellCoords)
                 else:
                     self.setCell((r, c), int(value)) # convert values to ints
+                    if self.forwardCheck:
+                        self.removeFromRemaining((r, c), int(value))
                 c += 1
+            # for
             r += 1
+        # for
 
-        # fill grid of remaining possible values for forward checking
-        # self.remaining = []
+    def removeFromRemaining(self, selectedCell, value):
+        r = selectedCell[0]
+        c = selectedCell[1]
+
+        for i in range(9):
+            # print(self.remaining[r][i])
+            self.remaining[r][i] = [x for x in self.remaining[r][i] if x != value]
+            # print(self.remaining[r][i])
+            self.remaining[i][c] = [x for x in self.remaining[i][c] if x != value]
+        # for
+
+        # remove all possible values from selectedCell
+        self.remaining[r][c] = []
+
+        # remove as possible value for entire block
+
+        # find top-left cell of block
+        blockX = floor(r/3)*3
+        blockY = floor(c/3)*3
+
+        for i in range(3):
+            for j in range(3):
+                self.remaining[blockX + i][blockY + j] = [x for x in self.remaining[blockX + i][blockY + j] if x != value]
+            # for
         # for
 
     def display(self):
@@ -42,6 +81,22 @@ class Puzzle(object):
                     print('_ ', end='')
                 else:
                     print(str(value)+' ', end='')
+            # for
+            print('\n', end='')
+        # for
+
+    def displayRemaining(self):
+        print("   0 1 2 3 4 5 6 7 8\n")
+
+        i = 0
+        for row in self.remaining:
+            print(i,' ', end='')
+            i += 1
+            for value in row:
+                if len(value) == 0:
+                    print('_ ', end='')
+                else:
+                    print(str(len(value))+' ', end='')
             # for
             print('\n', end='')
         # for
@@ -138,18 +193,35 @@ def backtrackingSearch(puzzle):
     return False
 #************************* def backtrackingSearch **********************************************
 
-puzzle = Puzzle()
+# default arguments
+forwardCheck = False
+inputFile = "easy.txt"
 
 # parse command line arguments
-inputFile = "easy.txt"
 if len(sys.argv) > 1:
-    inputFile = sys.argv[1] + ".txt"
+    forwardCheck = sys.argv[1]
+if len(sys.argv) > 2:
+    inputFile = sys.argv[2] + ".txt"
+
+puzzle = Puzzle(forwardCheck)
 
 # read input from file to list
 inputPuzzle = open(inputFile)
 puzzle.fill( inputPuzzle )
 
 # main
+
+# checks puzzle.remaining against puzzle.assignments
+# for i in range(9):
+#     for j in range(9):
+#         if len(puzzle.remaining[i][j]) == 0 and puzzle.assignments[i][j] == '':
+#             print(i,j)
+#         else:
+#             print('good')
+
+if puzzle.forwardCheck:
+    puzzle.displayRemaining()
+    print('')
 puzzle.display()
 completedPuzzle = backtrackingSearch( puzzle )
 
