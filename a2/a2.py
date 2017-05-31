48dcaac236c39835c4c95e6d57068b271e009513
Liam Ozog <lozog@uwaterloo.ca>
2017-05-31 19:33:42 -0400

    chore: created a2

---
+++ a2/a2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def restrict(factor, variable, value):
+    print("TODO: implement")
+
+def multiply(factor1, factor2):
+    print("TODO: implement")
+
+def sumout(factor, variable):
+    print("TODO: implement")
+
+def normalize(factor):
+    print("TODO: implement")
+
+def inference(factorList, queryVariables, orderedListOfHiddenVariables, evidenceList):
+    print("TODO: implement")
