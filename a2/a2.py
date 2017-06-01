33f726007c602eea4d7d78213798fb20a7239cd5
Liam Ozog <lozog@uwaterloo.ca>
2017-06-01 17:21:44 -0400

    fix: properly iterate over all elements in normalize function. feat: first draft of inference function

--- a2/a2.py
+++ a2/a2.py
@@ -23,16 +23,41 @@ def sumout(factor, variable):
 
 def normalize(factor):
     totalSum = factor.sum()
-    for row in factor:
-        for elem in row:
-            elem /= totalSum
+    for elem in np.nditer(factor, op_flags=['readwrite']):
+        elem /= totalSum
     return factor
 
+def _multiplyList(factorList):
+    if len(factorList) == 1:
+        return factorList[0]
+    elif len(factorList) == 2:
+        return multiply(factorList[0], factorList[1])
+    else:
+        return multiply(factorList[0], factorList[1:])
+
 def inference(factorList, queryVariables, orderedListOfHiddenVariables, evidenceList):
-    # for Q in queryVariables
-    print("TODO: implement")
+    for factor in factorList:
+        for idx,evidence in evidenceList:
+            factor = restrict(factor, idx, evidence)
+        # for
+    # for
 
+    for var in orderedListOfHiddenVariables:
+        # multiply all factors that have var in them
+        factorsToMultiply = []
+        for factor in factorList:
+            if factor.shape[var] > 1:
+                factorsToMultiply.append(factor)
+        # for
 
+        factorList = [f for f in factorList if f not in factorsToMultiply]
+        product = _multiplyList(factorsToMultiply)
+        summed = sumout(product, var)
+        factorList.append(summed)
+    # for
+
+    product = _multiplyList(factorList)
+    return normalize(product)
 
 ####### Tests for restrict
 
@@ -41,6 +66,7 @@ def inference(factorList, queryVariables, orderedListOfHiddenVariables, evidence
 # print(factor)
 # print('')
 # factor = restrict(factor, 0, 1)
+
 # print(factor)
 # print('')
 # factor = restrict(factor, 1, 1)
@@ -54,6 +80,13 @@ def inference(factorList, queryVariables, orderedListOfHiddenVariables, evidence
 # print(factor1)
 # print(factor2)
 # print(multiply(factor1, factor2))
+
+# f1 = np.array([0.9, 0.1])
+# f2 = np.array([0.91, 0.14])
+# f3 = multiply(f1, f2)
+# print(f3)
+# f4 = normalize(f3)
+# print(f4)
 
 ####### Tests for sumout
 
