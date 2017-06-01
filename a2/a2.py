7c2242baa35be969ac1bc7d61f5b7c9651f9d962
Liam Ozog <lozog@uwaterloo.ca>
2017-06-01 02:09:50 -0400

    feat: restrict function

--- a2/a2.py
+++ a2/a2.py
@@ -1,7 +1,12 @@
 import numpy as np
 
+# with help from Briuaku's answer:
+# https://stackoverflow.com/questions/24432209/python-index-an-array-using-the-colon-operator-in-an-arbitrary-dimension
 def restrict(factor, variable, value):
-    print("TODO: implement")
+    # take the (value)-th value of the (variable)-th dimension
+    slice_index = [slice(None)] * factor.ndim
+    slice_index[variable] = value
+    return factor[ tuple(slice_index) ]
 
 def multiply(factor1, factor2):
     print("TODO: implement")
@@ -14,3 +19,12 @@ def normalize(factor):
 
 def inference(factorList, queryVariables, orderedListOfHiddenVariables, evidenceList):
     print("TODO: implement")
+
+##*********************##
+## Test for restrict
+
+factor = np.arange(8).reshape(2,2,2)
+print(factor)
+print(restrict(factor, 1, 0))
+
+##********************************##
